92e1a179055b03885752b586e91235ed5af39757
Nimertzeth <77589721+Nimertzeth@users.noreply.github.com>
2021-02-26 19:30:15 +0300

    Правки скриптов, добавление класса visually-hidden

--- js/modal.js
+++ js/modal.js
@@ -30,3 +30,18 @@
     	}
     $('.modal-seach').fadeOut();
 });	
+
+    function feedbackForm(){
+        var x = document.getElementById("feedback-form");
+        x.style.display = "block"; 
+        }
+    function closeForm(){
+        var x = document.getElementById("feedback-form");
+        x.style.display = "none"; 
+        }   
+    $(document).click( function(e){
+        if ( $(e.target).closest('.button-contacts').length || $(e.target).closest('.feedback-form').length ) {
+        return;
+        }
+    $('.feedback-form').fadeOut();
+}); 
